24c1eae109b0e73a4e78279e8dfc9a0e5fe61a1e
Archer <arseniy.tarapon@gmail.com>
2019-03-28 13:22:49 +0600

    Установил пакет react-redux, командой npm i -save react-redux. Добавил контейнер Calcutator и вывел его в app.js. Создал redux хранилище и подключил его к моему приложению.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React, { Component } from 'react';
+import Calculator from "./containers/Calculator/Calculator";
 
 class App extends Component {
   render() {
-    return <div className='calculator'>
-
-    </div>
+    return <Calculator/>
   }
 }
 
---
+++ src/containers/Calculator/Calculator.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Calculator extends Component {
+    render() {
+        return (
+            <div className='Calculator'>
+                <input type="text"/>
+                <button>9</button>
+                <button>8</button>
+                <button>7</button>
+            </div>
+        )
+    }
+}
+
+export default Calculator;
